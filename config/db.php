37c06287fb55d5d1b6a140e4eb4aaaf7f913e5be
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-09 16:30:31 +0100

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname   = getenv('DB_NAME');
+
+$conn = new mysqli($hostname, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
