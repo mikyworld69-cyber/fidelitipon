26da1e1ef786acb06462d21dbb559d909be117b8
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-09 16:16:00 +0100

    Update cupones.php

--- public/admin/cupones.php
+++ public/admin/cupones.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-require_once __DIR__ . "/../config/db.php";
+require_once __DIR__ . '/../../config/db.php';
+
 
 if (!isset($_SESSION["admin_id"])) {
     header("Location: login.php");
