0732c41f19439da5d22a68a1cd9ed67d9a3930af
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-10 07:02:53 +0100

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$hostname = 'database-5018992042.webspace-host.com';
+$hostname = getenv('DB_HOST'); //database-5018992042.webspace-host.com
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
 $dbname   = getenv('DB_NAME');
