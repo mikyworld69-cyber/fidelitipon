cbf3ef4b81442b987eaa76c662457e3baaabf77c
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-10 08:28:19 +0100

    Create notificaciones.js

---
+++ public/push/notificaciones.js
@@ -0,0 +1,37 @@
+console.log("JS de notificaciones cargado.");
+
+const PUBLIC_VAPID = "BA4M737w3LmyAiXYmDwOihcwEflN-o9Axjz7wBBlo7ICzjhURi6EoqRpOA9phRgpaKTOuKzNlNCl2n8y2M632UI";
+
+if ("serviceWorker" in navigator && "PushManager" in window) {
+    
+    navigator.serviceWorker.register("/service-worker.js")
+    .then(reg => {
+        console.log("SW registrado:", reg);
+
+        return Notification.requestPermission().then(permission => {
+            if (permission !== "granted") {
+                console.log("Notificaciones no permitidas");
+                return;
+            }
+
+            return reg.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: PUBLIC_VAPID
+            });
+        });
+    })
+    .then(sub => {
+        if (!sub) return;
+        console.log("Suscripción creada:", sub);
+
+        return fetch("/push/guardar_suscripcion_push.php", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(sub)
+        });
+    })
+    .catch(err => console.error("Error:", err));
+
+} else {
+    console.log("SW o Push no soportado.");
+}
