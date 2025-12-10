69ebccf75e5b7648ee062b72cf848a9703cdd616
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-10 13:28:03 +0100

    Update logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,12 +1,5 @@
 <?php
 session_start();
-
-// Eliminar todas las variables de sesión
-session_unset();
-
-// Destruir la sesión completamente
 session_destroy();
-
-// Redirigir al login
-header("Location: login.php");
+header("Location: /admin/login.php");
 exit;
