83948d0ebcbfca7397771272c9adfb13e87bb761
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-11 05:35:31 +0100

    Update login.php

--- public/app/login.php
+++ public/app/login.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/db.php';
-
+require_once __DIR__ . '/../../config/db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = trim($_POST['email']);
