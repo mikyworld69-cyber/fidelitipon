bcca3d03d4e349faf1cabf7733fa62d830565dcb
mikyworld69-cyber <mikyworld69@gmail.com>
2025-12-12 13:49:48 +0100

    Update qrlib.php

--- lib/phpqrcode/qrlib.php
+++ lib/phpqrcode/qrlib.php
@@ -1,68 +1,47 @@
 <?php
-/*
- * PHP QR Code Lite - Versión simplificada funcional.
- * Genera códigos QR válidos, sin dependencias adicionales.
- */
 
-class QRcode {
-
-    public static function png($text, $outfile = false, $level = QR_ECLEVEL_L, $pixelSize = 10, $margin = 4) {
-
+class QR_SVG
+{
+    public static function generate($text, $outfile = null, $size = 6)
+    {
         $matrix = self::encodeMatrix($text);
 
-        $size = count($matrix);
-        $imgSize = ($size * $pixelSize) + ($margin * 2);
-        $img = imagecreatetruecolor($imgSize, $imgSize);
+        $svg  = '<?xml version="1.0" standalone="no"?>' . "\n";
+        $svg .= '<svg xmlns="http://www.w3.org/2000/svg" width="'.(count($matrix)*$size).'" height="'.(count($matrix)*$size).'" shape-rendering="crispEdges">' . "\n";
+        $svg .= '<rect width="100%" height="100%" fill="white"/>' . "\n";
 
-        $white = imagecolorallocate($img, 255, 255, 255);
-        $black = imagecolorallocate($img, 0, 0, 0);
-        imagefill($img, 0, 0, $white);
-
-        for ($y = 0; $y < $size; $y++) {
-            for ($x = 0; $x < $size; $x++) {
-                if ($matrix[$y][$x] === 1) {
-                    imagefilledrectangle(
-                        $img,
-                        $x * $pixelSize + $margin,
-                        $y * $pixelSize + $margin,
-                        ($x + 1) * $pixelSize + $margin - 1,
-                        ($y + 1) * $pixelSize + $margin - 1,
-                        $black
-                    );
+        for ($y = 0; $y < count($matrix); $y++) {
+            for ($x = 0; $x < count($matrix); $x++) {
+                if ($matrix[$y][$x]) {
+                    $svg .= '<rect x="'.($x*$size).'" y="'.($y*$size).'" width="'.$size.'" height="'.$size.'" fill="black"/>' . "\n";
                 }
             }
         }
 
+        $svg .= '</svg>';
+
         if ($outfile) {
-            imagepng($img, $outfile);
-        } else {
-            header("Content-Type: image/png");
-            imagepng($img);
+            file_put_contents($outfile, $svg);
+            return true;
         }
 
-        imagedestroy($img);
+        header("Content-Type: image/svg+xml");
+        echo $svg;
+        return true;
     }
 
-    private static function encodeMatrix($text) {
-        // Convertimos el texto a un hash reproducible
+    private static function encodeMatrix($text)
+    {
         $hash = sha1($text);
-
-        // Tamaño fijo de matriz (versión simplificada)
         $size = 33;
+
         $matrix = array_fill(0, $size, array_fill(0, $size, 0));
-
-        $bin = '';
-        foreach (str_split($hash) as $char) {
-            $bin .= str_pad(base_convert(ord($char), 10, 2), 8, "0", STR_PAD_LEFT);
-        }
-
-        $binArr = str_split($bin);
-        $totalBits = count($binArr);
+        $bits = str_split($hash);
 
         $i = 0;
         for ($y = 0; $y < $size; $y++) {
             for ($x = 0; $x < $size; $x++) {
-                $matrix[$y][$x] = ( (int)$binArr[$i % $totalBits] === 1 ) ? 1 : 0;
+                $matrix[$y][$x] = (ord($bits[$i % strlen($hash)]) % 2);
                 $i++;
             }
         }
@@ -70,8 +49,3 @@ class QRcode {
         return $matrix;
     }
 }
-
-define('QR_ECLEVEL_L', 0);
-define('QR_ECLEVEL_M', 1);
-define('QR_ECLEVEL_Q', 2);
-define('QR_ECLEVEL_H', 3);
